25703169f6c736302ba0685703adb569cf176a53
AndreyUrazaev <f.a.z.a@bk.ru>
2023-10-28 15:09:53 +0500

    ReturningStrings

---
+++ 8kyu/ReturningStrings.js
@@ -0,0 +1,27 @@
+// Returning Strings
+//
+// Make a function that will return a greeting statement that uses an input;
+// your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
+
+// const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+
+// -----
+// function greet(name){
+//     return "Hello, <name> how are you doing today?".replace("<name>", name);
+// }
+
+// -----
+// function greet(name){
+//     let a= name.split("");
+//     let j=[];
+//     a[0]="Hello,"
+//     a[1]=name
+//     a[2]="how are you doing today?"
+//     j.push(a[0],a[1],a[2])
+//     return j.join(" ")
+// }
