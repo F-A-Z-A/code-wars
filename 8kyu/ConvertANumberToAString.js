da378a04c676861280bb2cff3ed1d843802e903e
AndreyUrazaev <f.a.z.a@bk.ru>
2023-11-29 16:49:16 +0500

    ConvertANumberToAString

---
+++ 8kyu/ConvertANumberToAString.js
@@ -0,0 +1,19 @@
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+const numberToString = (num) => `${num}`;
+console.log(numberToString(123))
+// '123'
+
+
+//---
+// function numberToString(num) {
+// 	return ''+num;
+// }
+
+//---
+// const numberToString = num => num.toString();
