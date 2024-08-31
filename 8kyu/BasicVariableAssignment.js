73122b4d1984e187c8f4c432d70422b046dea19b
AndreyUrazaev <f.a.z.a@bk.ru>
2024-08-31 12:58:55 +0500

    8-RemoveDuplicatesFromList

--- 8kyu/BasicVariableAssignment.js
+++ 8kyu/BasicVariableAssignment.js
@@ -1,5 +1,5 @@
 // This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
 
-var a = "code";
+var arr = "code";
 var b = "wa.rs";
-var name = a + b;
+var name = arr + b;
